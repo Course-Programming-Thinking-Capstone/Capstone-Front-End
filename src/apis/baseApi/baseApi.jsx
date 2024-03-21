b5a9d7f856388dece2313fb19a86ddbda9b952ff
Blue Teeth <daominhtri1000@gmail.com>
2024-03-21 16:50:06 +0700

    Add axios; Update api structure

---
+++ src/apis/baseApi/baseApi.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = "https://www.kidpro-production.somee.com/";
+
+const instance = axios.create({
+  baseURL: url,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// config request Authenticate header
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
