f86dcd9e077906cc76a3c97aa1b19d44cf4b6760
Gorst0902 <dinhvanthanhan0902@gmail.com>
2024-04-03 23:07:08 +0700

    style and navigate between StaffOrder and StaffOrderDetail successfully

--- src/markup/Pages/Staff/StaffOrder/StaffOrderDetail.js
+++ src/markup/Pages/Staff/StaffOrder/StaffOrderDetail.js
@@ -1,83 +1,100 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import momo from "../../../../images/icon/momo.png"
+import { useNavigate } from 'react-router-dom';
 
-const SuccessOrder = () => {
+const SuccessOrder = ({ orderDetail }) => {
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate(-1);
+    };
     return (
-        <div className='staff-order-detail'>
-            <h3>Order detail</h3>
-            <div className='d-flex justify-content-between'>
-                <div className="d-flex justify-content-start">
-                    <i class="fa-solid fa-user"></i>
-                    <p className="mb-0 ms-1">Parent: </p>
-                    <span className='ms-2'></span>
-                </div>
-                <div>
-                    <span>Success</span>
-                </div>
-            </div>
-            <div>
-                <div className="d-flex justify-content-start">
-                    <i class="fa-solid fa-user"></i>
-                    <p className="mb-0 ms-1">Teacher: </p>
-                    <span className='ms-2'></span>
-                </div>
-                <hr />
-                <div className='d-flex justify-content-between'>
-                    <img src="" alt="day la hinh anh lop hoc" />
-                    <p className='mb-0'></p>
-                    <p>Quantity: <span></span></p>
-                    <p className='orange'></p>
-                </div>
-            </div>
-            <div className='d-flex'>
-                <div>
-                    <p>Order ID: <span></span></p>
-                    <p>Order date: <span></span></p>
-                </div>
-                <div className='d-flex'>
-                    <img src="" alt="momo" />
-                    <p>Pay with momo e-wallet</p>
-                </div>
-            </div>
+        <div className='staff-order-detail mt-3 mx-5 py-3 px-5' style={{ backgroundColor: 'white' }}>
             <div className="d-flex justify-content-between">
-                <div style={{ width: '45%' }}>
-                    <p>Number of students selected: <span></span></p>
-                    <div className='d-flex justify-content-center'>
-                        <div style={{ width: '50%' }}></div>
+                <h2 className='orange mb-1'>Order detail</h2>
+                <div>
+                    <button onClick={handleBackClick} style={{ backgroundColor: '#7F7C7C', color: 'white', border: 'none', marginRight: '10px', borderRadius: '5px' }}>Back</button>
+                </div>
+            </div>
+            <div className='d-flex justify-content-between py-2 px-3' style={{ backgroundColor: '#eceace', borderRadius: '8px', height: '50px' }}>
+                <div className="d-flex justify-content-start">
+                    <i style={{ fontSize: '22px' }} class="fa-solid fa-user orange mt-2"></i>
+                    <p className="mb-0 ms-3 mt-2" style={{ fontSize: '18px' }}>Parent: </p>
+                    <span className="mb-0 ms-3 mt-2" style={{ fontSize: '18px' }}>{orderDetail.parentName}</span>
+                </div>
+                <div className='text-center' style={{ width: '100px', height: '35px' }}>
+                    <div className='pt-1' style={{ backgroundColor: '#1A9CB7', borderRadius: '8px', color: 'white', width: '90px', height: '30px' }}>Success</div>
+                </div>
+            </div>
+            <div className='mt-3 py-2 px-3' style={{ backgroundColor: '#eceace', borderRadius: '8px' }}>
+                <div className="d-flex justify-content-start">
+                    <i style={{ fontSize: '22px' }} class="fa-solid fa-user orange mt-2"></i>
+                    <p className="mb-0 ms-3 mt-2" style={{ fontSize: '18px' }}>Teacher: </p>
+                    <span className="mb-0 ms-3 mt-2" style={{ fontSize: '18px' }}>{orderDetail.parentName}</span>
+                </div>
+                <hr className='my-1' />
+                <div className='d-flex justify-content-around' style={{ fontSize: '16px' }}>
+                    <img src={orderDetail.pictureUrl} style={{ height: "80px", width: '80px' }} />
+                    <p className='mb-0'>{orderDetail.courseName}</p>
+                    <p>Quantity: <span>{orderDetail.quantityPurchased}</span></p>
+                    <p className='orange'>{orderDetail.totalPrice} đ</p>
+                </div>
+            </div>
+            <div className='d-flex mt-3 py-2 px-3' style={{ backgroundColor: '#eceace', borderRadius: '8px' }}>
+                <div style={{ width: "60%" }}>
+                    <p className='mb-1'>Order ID: <span>{orderDetail.orderId}</span></p>
+                    <p className='mb-1'>Order date: <span>{orderDetail.orderDate}</span></p>
+                </div>
+                <div className='d-flex' style={{ width: "40%" }}>
+                    <img style={{ height: '50px', width: '50px' }} src={momo} />
+                    <p className='mt-2 ms-2'>Pay with momo e-wallet</p>
+                </div>
+            </div>
+            <div className="d-flex justify-content-between mt-3">
+                <div style={{ width: '45%' }} className='py-2 px-3'>
+                    <p style={{ backgroundColor: '#ff8a00', color: 'white', fontSize: '17px', borderRadius: '8px 8px 0px 0px' }} className='mb-0 ps-3 py-1'>Number of students selected: <span></span></p>
+                    <div className='d-flex justify-content-center px-4'>
+                        {orderDetail.students.map((student) => (
+                            < div className='text-center py-1 my-1' style={{ width: '50%', borderRadius: '8px', border: '1px solid #ff8a00' }} key={student.studentId}>{student.studentName}</div>
+                        ))}
                     </div>
-                    <p>Student account will send to</p>
-                    <div className='d-flex justify-content-center'>
-                        <div></div>
+
+                    <div className='mt-4'>
+                        <p style={{ backgroundColor: '#ff8a00', color: 'white', fontSize: '17px', borderRadius: '8px 8px 0px 0px' }} className='mb-0 ps-3 py-1'>Student account will send to</p>
+                        <div className='d-flex justify-content-center'>
+                            <div className='text-center py-1 my-1 px-2' style={{ borderRadius: '8px', border: '1px solid #ff8a00' }} v>{orderDetail.parentEmail}</div>
+                        </div>
                     </div>
                 </div>
-                <div style={{ width: '45%' }}>
-                    <p>Order information</p>
-                    <div>
-                        <div className="d-flex justify-content-between">
+                <div style={{ width: '45%' }} className='py-2 px-3'>
+                    <p style={{ backgroundColor: '#ff8a00', color: 'white', fontSize: '17px', borderRadius: '8px 8px 0px 0px' }} className='mb-1 ps-3 py-1'>Order information</p>
+                    <div className='px-4'>
+                        <div className="d-flex justify-content-between mb-2">
                             <span>Course</span>
-                            <span></span>
+                            <span>{orderDetail.courseName}</span>
                         </div>
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between mb-2">
                             <span>Price</span>
-                            <span></span>
+                            <span>{orderDetail.price}</span>
                         </div>
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between mb-2">
                             <span>Quantity</span>
-                            <span></span>
+                            <span>{orderDetail.quantityPurchased}</span>
                         </div>
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between mb-2">
                             <span>Discount</span>
-                            <span></span>
+                            <span>0</span>
                         </div>
                         <hr />
-                        <div className="d-flex justify-content-between">
+                        <div className="d-flex justify-content-between mb-2">
                             <span>Total</span>
-                            <span className='orange'></span>
+                            <span className='orange'>{orderDetail.totalPrice}</span>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
@@ -140,9 +157,9 @@ export default function StaffOrderDetail() {
 
     return (
         <div>
-            {orderDetail.status === 'Success' && <SuccessOrder />}
-            {orderDetail.status === 'Pending' && <div>Pending Order Details</div>}
-            {orderDetail.status === 'Refunded' && <div>Refunded Order Details</div>}
+            {orderDetail.status === 'Success' && <SuccessOrder orderDetail={orderDetail} />}
+            {orderDetail.status === 'Pending' && <PendingOrder orderDetail={orderDetail} />}
+            {orderDetail.status === 'Refunded' && <RefundedOrder orderDetail={orderDetail} />}
         </div>
     )
 }
