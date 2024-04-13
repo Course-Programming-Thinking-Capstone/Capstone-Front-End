8ed170679fe25bd112672c560b8fee722af797fa
Gorst0902 <dinhvanthanhan0902@gmail.com>
2024-04-13 11:38:00 +0700

    pull main to an-dev

--- src/markup/Pages/VerifyEmail.js
+++ src/markup/Pages/VerifyEmail.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import background from './../../images/background/loginBackground.webp';
 import gmail from './../../images/icon/gmail.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export default function VerifyEmail() {
+    const navigate = useNavigate();
+
+    const navigateToLogin = () => {
+        navigate("/login");
+    }
     return (
         <div style={{
             backgroundImage: `url(${background})`,
@@ -20,9 +26,9 @@ export default function VerifyEmail() {
                 <div className="d-flex justify-content-center">
                     <img style={{ height: '200px', width: '200px' }} className='img-responsive' src={gmail} alt="" />
                 </div>
-                <p>To complete your profile and start taking business rides with KidsPro, you'll need to verify your email address. </p>
+                <p>To complete your profile and start taking business rides with KidsPro, please check your email to verify your account. </p>
                 <div className="d-flex justify-content-center">
-                    <button onClick={() => window.open("https://mail.google.com/mail/", "_blank")}>VERIFY</button>
+                    <button onClick={navigateToLogin}>LOGIN NOW</button>
                 </div>
             </div>
         </div>
