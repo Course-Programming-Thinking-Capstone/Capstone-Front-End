02d0a2e3698eb5a12e7a5e805efe15a27b27a56d
Blue Teeth <daominhtri1000@gmail.com>
2024-05-01 00:39:10 +0700

    Update notification css

--- src/markup/Layout/Components/Notification/ConfirmModal.jsx
+++ src/markup/Layout/Components/Notification/ConfirmModal.jsx
@@ -1,6 +1,6 @@
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import "./Notification.css";
+import { Button, Stack } from '@mui/material';
 
 export const ConfirmModal = ({ title, message, closeLabel, acceptLabel, handleAccept, handleDeny, show }) => {
 
@@ -14,12 +14,12 @@ export const ConfirmModal = ({ title, message, closeLabel, acceptLabel, handleAc
                 centered
                 className='notification-modal'
             >
-                {title &&
+                {/* {title &&
                     (<Modal.Header closeButton>
                         <Modal.Title>{title}</Modal.Title>
                     </Modal.Header>)
-                }
-                <Modal.Body>
+                } */}
+                {/* <Modal.Body>
                     {message ?? "Modal body"}
                 </Modal.Body>
                 <Modal.Footer>
@@ -27,7 +27,38 @@ export const ConfirmModal = ({ title, message, closeLabel, acceptLabel, handleAc
                         {closeLabel ?? "Close"}
                     </Button>
                     <Button variant="primary" type='button' onClick={handleAccept}>{acceptLabel ?? "Ok"}</Button>
-                </Modal.Footer>
+                </Modal.Footer> */}
+
+                {message && (
+                    <>
+                        <Modal.Body>
+                            <Stack
+                                spacing={2}
+                                alignContent="center"
+                                justifyContent="center"
+                                direction="column"
+                                sx={{ fontSize: "1.3rem", color: "#6DCE63", textAlign: 'center' }}
+                            >
+                                <p>{message}</p>
+                            </Stack>
+
+                            <Stack
+                                direction="row"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                spacing={2}
+                                sx={{ fontSize: "1rem", margin: "2rem 4rem 0" }}
+                            >
+                                <Button variant='outlined' color='error' onClick={handleDeny}>
+                                    {closeLabel ?? "Close"}
+                                </Button>
+                                <Button variant='contained' color='primary' onClick={handleAccept}>
+                                    {acceptLabel ?? "Ok"}
+                                </Button>
+                            </Stack>
+                        </Modal.Body>
+                    </>
+                )}
             </Modal>
         </>
     );
--- src/markup/Layout/Components/Notification/ModalNotification.jsx
+++ src/markup/Layout/Components/Notification/ModalNotification.jsx
@@ -1,5 +1,7 @@
 import Modal from 'react-bootstrap/Modal';
 import "./Notification.css";
+import { Stack } from '@mui/material';
+import { CheckCircleOutline } from '@mui/icons-material';
 
 export const ModalNotification = ({ title, message, handleClose, show }) => {
 
@@ -10,14 +12,19 @@ export const ModalNotification = ({ title, message, handleClose, show }) => {
             centered
             className='notification-modal'
         >
-            {title &&
+            {/* {title &&
                 (<Modal.Header closeButton>
                     <Modal.Title>{title}</Modal.Title>
                 </Modal.Header>)
+            } */}
+            {message &&
+                <Modal.Body>
+                    <Stack spacing={2} sx={{ fontSize: "1.3rem", color: "rgb(46, 125, 50)" }} direction={"row"} alignItems={"center"} justifyContent={"flex-start"}>
+                        <CheckCircleOutline />
+                        <p>{message}</p>
+                    </Stack>
+                </Modal.Body>
             }
-            <Modal.Body>
-                {message ?? "Success"}
-            </Modal.Body>
         </Modal>
     );
 }
